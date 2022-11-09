c084b739e2c76ad9083b261b2db7d4346c16899c
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-09 16:47:15 +0300

    api controller completed

--- src/main/java/com/halleluhya/halleluhya/controllers/Hallecontroller.java
+++ src/main/java/com/halleluhya/halleluhya/controllers/Hallecontroller.java
@@ -1,10 +1,9 @@
 package com.halleluhya.halleluhya.controllers;
 
 import com.halleluhya.halleluhya.models.Halleluhya;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,5 +18,30 @@ public class Hallecontroller {
       halleluhyas.add(new Halleluhya(2,"diana", "web developer"));
       halleluhyas.add(new Halleluhya(3,"fiona", "advocate"));
       return ResponseEntity.ok(halleluhyas);
+    }
+
+    @GetMapping("/Halleluhya/{id}")
+    public Halleluhya HalleluhyaDetail(@PathVariable int id, String name, String occupation){
+        return new Halleluhya(id, "diana", "integration engineer");
+    }
+
+    @PostMapping("/Halleluhya/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Halleluhya> createHalleluhya(@RequestBody Halleluhya halleluhya){
+        System.out.println(halleluhya.getName());
+        System.out.println(halleluhya.getOccupation());
+        return new ResponseEntity<>(halleluhya, HttpStatus.CREATED);
+    }
+
+    @PutMapping("Halleluhya/{id}/update")
+    public ResponseEntity<Halleluhya> updateHalleluhya(@RequestBody Halleluhya halleluhya, @PathVariable("id") int halleid){
+        System.out.println(halleluhya.getName());
+        System.out.println(halleluhya.getOccupation());
+        return ResponseEntity.ok(halleluhya);
+    }
+    @DeleteMapping("Halleluhya/{id}/delete")
+    public ResponseEntity<String> deletehalleluhya(@PathVariable("id") int haleid){
+        System.out.println(haleid);
+        return ResponseEntity.ok("halleluhya deleted succesfully");
     }
 }
