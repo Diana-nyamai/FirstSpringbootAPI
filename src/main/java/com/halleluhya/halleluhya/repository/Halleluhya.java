7d730777c53b04ca8b91e1708891dcd4f0942a13
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-09 17:43:33 +0300

    creation of a repository

---
+++ src/main/java/com/halleluhya/halleluhya/repository/Halleluhya.java
@@ -0,0 +1,7 @@
+package com.halleluhya.halleluhya.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Halleluhya extends JpaRepository<Halleluhya, Integer> {
+
+}
