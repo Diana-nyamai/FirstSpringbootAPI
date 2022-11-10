6ebd2e45a74ed60900fd915103d7ed7f68f74480
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-10 14:20:19 +0300

    Hallecontroller

--- src/main/java/com/halleluhya/halleluhya/controllers/Hallecontroller.java
+++ src/main/java/com/halleluhya/halleluhya/controllers/Hallecontroller.java
@@ -1,6 +1,9 @@
 package com.halleluhya.halleluhya.controllers;
 
+import com.halleluhya.halleluhya.dto.HalleluhyaDto;
 import com.halleluhya.halleluhya.models.Halleluhya;
+import com.halleluhya.halleluhya.services.HalleluhyaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +14,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/")
 public class Hallecontroller {
+    private HalleluhyaService halleluhyaService;
+    @Autowired
+    public Hallecontroller(HalleluhyaService halleluhyaService) {
+        this.halleluhyaService = halleluhyaService;
+    }
+
     @GetMapping("Halleluhya")
     public ResponseEntity<List<Halleluhya>> getHalleluhyas(){
       List<Halleluhya> halleluhyas = new ArrayList<>();
@@ -26,11 +35,13 @@ public class Hallecontroller {
     }
 
     @PostMapping("/Halleluhya/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Halleluhya> createHalleluhya(@RequestBody Halleluhya halleluhya){
-        System.out.println(halleluhya.getName());
-        System.out.println(halleluhya.getOccupation());
-        return new ResponseEntity<>(halleluhya, HttpStatus.CREATED);
+    @ResponseStatus(HttpStatus.CREATED)////)
+//    ResponseEntity<Halleluhya> (@RequestBody Halleluhya halleluhya)
+    public ResponseEntity<HalleluhyaDto> createHalleluhya(@RequestBody HalleluhyaDto halleluhyaDto){
+//        System.out.println(halleluhya.getName());
+//        System.out.println(halleluhya.getOccupation());
+//        return new ResponseEntity<>(halleluhya, HttpStatus.CREATED);
+        return new ResponseEntity<>(halleluhyaService.createHalleluhya(halleluhyaDto), HttpStatus.CREATED);
     }
 
     @PutMapping("Halleluhya/{id}/update")
