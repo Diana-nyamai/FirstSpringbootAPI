05138101a6468bb5477973670a6b5e992b0e30c6
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-10 14:22:59 +0300

    HalleluhyaService

---
+++ src/main/java/com/halleluhya/halleluhya/services/HalleluhyaService.java
@@ -0,0 +1,8 @@
+package com.halleluhya.halleluhya.services;
+
+import com.halleluhya.halleluhya.dto.HalleluhyaDto;
+
+public interface HalleluhyaService {
+    HalleluhyaDto createHalleluhya(HalleluhyaDto halleluhyaDto);
+
+}
