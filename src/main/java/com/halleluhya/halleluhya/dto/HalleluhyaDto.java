63ecd57f3dbf131e1e7235550344bff00b342a07
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-10 14:20:42 +0300

    HalleluhyaDto

---
+++ src/main/java/com/halleluhya/halleluhya/dto/HalleluhyaDto.java
@@ -0,0 +1,10 @@
+package com.halleluhya.halleluhya.dto;
+
+import lombok.Data;
+
+@Data
+public class HalleluhyaDto {
+    private int id;
+    private String name;
+    private String occupation;
+}
