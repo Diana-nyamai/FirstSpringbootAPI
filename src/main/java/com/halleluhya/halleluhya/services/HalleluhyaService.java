1ae314e37b7b5717c2c23a17ec91b5be02fe9577
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-10 18:57:31 +0300

    HalleluhyaService update

--- src/main/java/com/halleluhya/halleluhya/services/HalleluhyaService.java
+++ src/main/java/com/halleluhya/halleluhya/services/HalleluhyaService.java
@@ -7,4 +7,7 @@ import java.util.List;
 public interface HalleluhyaService {
     HalleluhyaDto createHalleluhya(HalleluhyaDto halleluhyaDto);
     List<HalleluhyaDto> getAllHalleluhya();
+    HalleluhyaDto getHalleluhyaid(int id);
+    HalleluhyaDto updatehalleluhya(HalleluhyaDto halleluhyaDto, int id);
+    void deletehalleid(int id);
 }
